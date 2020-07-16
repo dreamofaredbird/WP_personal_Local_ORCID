35bfd60b70f927d07c8a0731d511070fed78a6e3
Rob Blackburn <rjb@fastmail.com>
2020-07-16 13:02:16 +0100

    Header redirect properly
    adding back the proper header and modifying it to redirtect to QA

---
+++ wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:4:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1595269591;s:7:"authKey";s:64:"p|Kr?gp|by8cQO~=kck+I,co]J5_R7If9j8Ez=%O=wJiF]0V8XxmVA=T#`<0+%4f";}
