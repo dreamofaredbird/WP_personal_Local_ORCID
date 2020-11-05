1a7607e0e353f28827e42c509e284dc1c30ec4c9
Dan Dineen <dan@karyx.co.uk>
2020-11-05 09:23:30 +0000

    Sort the header and download buttons
    - Style the download buttons across the site
    - Make a small change in functions.php to remove the site title
    - Some CSS tidying

--- themes/orcid-outreach-pro/functions.php
+++ themes/orcid-outreach-pro/functions.php
@@ -214,7 +214,7 @@ return str_replace( 'href="/', 'href="http://qa.orcid.org/', $html );
 //* Modify the header URL - HTML5 Version
 add_filter( 'genesis_seo_title', 'child_header_title', 10, 3 );
 function child_header_title( $title, $inside, $wrap ) {
-    $inside = sprintf( '<a href="https://qa.orcid.org/" title="%s">%s</a>', esc_attr( get_bloginfo( 'name' ) ), get_bloginfo( 'name' ) );
+    $inside = sprintf( '<a href="https://qa.orcid.org/" title="%s"></a>', esc_attr( get_bloginfo( 'name' ) ), get_bloginfo( 'name' ) );
     return sprintf( '<%1$s class="site-title">%2$s</%1$s>', $wrap, $inside );
 }
 
